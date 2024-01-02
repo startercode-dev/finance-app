256be2f203dd890e283b62b4f4761f2a15aa5b7a
hpk992 <83901776+hpk992@users.noreply.github.com>
2024-01-01 23:37:32 -0800

    logout

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,10 +1,17 @@
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { getUserData } from '@/store/userActions';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export default function Dashboard() {
     const dispatch = useAppDispatch();
     const user = useAppSelector((state) => state.user);
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        await fetch('/api/logout');
+        router.push('/');
+    };
 
     useEffect(() => {
         if (user.isAuth === false) {
@@ -16,8 +23,9 @@ export default function Dashboard() {
         <>
             <h1>dashboard</h1>
             <h2>{user.isAuth ? 'auth' : 'not auth'}</h2>
+            <button type="button" onClick={handleLogout}>
+                Logout
+            </button>
         </>
     );
 }
-
-//! Look into using getServersideprops for this as well, so the first render won't be 'not auth'
