54b6abd0438e3bc3987f19352d67c37dcc16d455
hpk992 <83901776+hpk992@users.noreply.github.com>
2024-01-01 23:37:32 -0800

    conditional index 2

---
+++ client/src/utils/authenticate.ts
@@ -0,0 +1,14 @@
+import { jwtVerify } from 'jose';
+
+export async function authenticate(token: string): Promise<boolean> {
+    const valid = await jwtVerify(
+        token,
+        new TextEncoder().encode(process.env.JWT_SECRET)
+    );
+
+    if (valid) {
+        return true;
+    } else {
+        return false;
+    }
+}
